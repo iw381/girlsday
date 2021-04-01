6aa154b6cfbe320b44d4825d2ddd7bdebd39641b
MartinaSonja <martinasonjazuendel@gmail.com>
2021-04-01 10:03:53 +0200

    correct errors, faster loading

--- girlsday.py
+++ girlsday.py
@@ -46,15 +46,15 @@ def plotEarth(earthMapMoll):
 
     #plotting map
     im = ax.imshow(earthMapMoll,vmin=-8000, vmax=6705, cmap='gist_earth')
-    
+
     #setting position and size of colorbar (xPos,yPos,width,height)
-    cax = fig.add_axes([0.32, 0.25, 0.4, 0.018]) 
+    cax = fig.add_axes([0.32, 0.25, 0.4, 0.018])
     cbar = fig.colorbar(im,orientation="horizontal",cax=cax)
     cbar.set_label('Höhe über N.N. [m]', size=24, labelpad=20)
     ax.axis('off')
-        
+
 def plotEarth3d(earthMapMoll,earth3d):
-    
+
     fig = plt.figure(figsize=(18,16))
     gs = fig.add_gridspec(1, 2,width_ratios=[1,4])
 
@@ -62,41 +62,41 @@ def plotEarth3d(earthMapMoll,earth3d):
     #ax1.gridlines(color='#333333', linestyle='dotted')
     #ax1.imshow(earthMapCartview, origin="upper", extent=(-180, 180, -90, 90),
     #      transform=ccrs.PlateCarree(),cmap='gist_earth',
-    #           vmin=-8000, vmax=6705) 
+    #           vmin=-8000, vmax=6705)
     ax1 = fig.add_subplot(gs[0,0])
-    ax1.imshow(earth3d) 
+    ax1.imshow(earth3d)
     ax1.axis('off')
 
     #plotting map
     ax2 = fig.add_subplot(gs[0,1])
     im = ax2.imshow(earthMapMoll,cmap='gist_earth',vmin=-8000, vmax=6705)
-   
+
     #setting position and size of colorbar (xPos,yPos,width,height)
-    cax = fig.add_axes([0.41, 0.3, 0.4, 0.018]) 
+    cax = fig.add_axes([0.41, 0.3, 0.4, 0.018])
     ticks = np.arange(-7500,5001,2500)
     cbar = fig.colorbar(im,orientation="horizontal",cax=cax,ticks=ticks)
     cax.tick_params(labelsize=20)
     cbar.set_label('Höhe über N.N. [m]', size=24, labelpad=20)
     ax2.axis('off')
 
-def plotPowerspectrum(clCur,LMAX,isCMB=False):    
+def plotPowerspectrum(clCur,LMAX,isCMB=False):
     fig = plt.figure(figsize=(12,10))
     ax = fig.add_subplot(111)
 
-    #split the ell and cl arrays it two halfes 
-    # s.t. they can be plotted in two different plot styles 
+    #split the ell and cl arrays it two halfes
+    # s.t. they can be plotted in two different plot styles
     ellLeq = ell[ell<=LMAX]
     clDimlessLeq = ellLeq* (ellLeq + 1) * clCur[ell<=LMAX]
     ellGeq = ell[ell>=LMAX]
     clDimlessGeq = ellGeq * (ellGeq + 1) * clCur[ell>=LMAX]
-    
+
     #plot the C_l power spectrum
     ax.plot(ellLeq, clDimlessLeq, c="#222222", lw=1.5)
     ax.plot(ellGeq, clDimlessGeq, c="#666666", lw=1.5)
-    
+
     # add the vertical line and shaded region
     ax.axvline(x=LMAX,lw=2)
-    xLim = np.array(ax.get_xlim())    
+    xLim = np.array(ax.get_xlim())
     xLim[1] = 210
     if (isCMB):
         xLim[1] = 1024
@@ -118,31 +118,29 @@ def plotPowerspectrum(clCur,LMAX,isCMB=False):
     plt.yticks(fontsize=16)
     ax.grid(True)
 
-def plotEarth_Powerspectrum(earthMap,clCur,LMAX): 
-    
-    #split the ell and cl arrays it two halfes 
-    # s.t. they can be plotted in two different plot styles 
+def plotEarth_Powerspectrum(earthMap,clCur,LMAX):
+
+    #split the ell and cl arrays it two halfes
+    # s.t. they can be plotted in two different plot styles
     ellLeq = ell[ell<=LMAX]
     clDimlessLeq = ellLeq* (ellLeq + 1) * clCur[ell<=LMAX]
     ellGeq = ell[ell>=LMAX]
     clDimlessGeq = ellGeq * (ellGeq + 1) * clCur[ell>=LMAX]
-    
+
     fig = plt.figure(figsize=(18,10),constrained_layout=False)
-    
-    
+
+
     heights = [1.5, 3, 1.5]
     gs1 = fig.add_gridspec(nrows=3, ncols=1, left=0.05, right=0.25,wspace=0.05, height_ratios=heights)
     ax1 = fig.add_subplot(gs1[1,0])
-    
 
-    
     #plot the C_l power spectrum
     ax1.plot(ellLeq, clDimlessLeq, c="#222222", lw=1.5)
     ax1.plot(ellGeq, clDimlessGeq, c="#666666", lw=1.5)
-    
+
     # add the vertical line and shaded region
     ax1.axvline(x=LMAX,lw=2)
-    xLim = np.array(ax1.get_xlim())    
+    xLim = np.array(ax1.get_xlim())
     xLim[1] = 210
     ax1.axvspan(LMAX, xLim[1], facecolor='0.2', alpha=0.2)
 
@@ -158,15 +156,15 @@ def plotEarth_Powerspectrum(earthMap,clCur,LMAX):
     plt.xticks([1,10,100],fontsize=16)
     plt.yticks(fontsize=16)
     ax1.grid(True)
-    
+
 
     gs2 = fig.add_gridspec(nrows=1, ncols=1, left=0.30, right=0.98,wspace=0.05)
     ax2 = fig.add_subplot(gs2[0,0])
 
     im = ax2.imshow(earthMap,cmap='gist_earth',vmin=-8000, vmax=6705)
-    
+
     #setting position and size of colorbar (xPos,yPos,width,height)
-    cax = fig.add_axes([0.434, 0.16, 0.4, 0.018]) 
+    cax = fig.add_axes([0.434, 0.16, 0.4, 0.018])
     ticks = np.arange(-7500,5001,2500)
     cbar = fig.colorbar(im,orientation="horizontal",cax=cax,ticks=ticks)
     cax.tick_params(labelsize=20)
@@ -189,12 +187,20 @@ LMAX = 1024
 cl = np.loadtxt("data/clEarth.dat")
 ell = np.arange(len(cl))
 
+preloaded_list_earth = []
+
+for i in range(1, 28):
+        LMAX_earth = int(np.exp(i/28.*5.3))
+
+        mollviewMapReconstr = np.load("data/mollviewEarth_recon_max%d.dat"%LMAX_earth,allow_pickle=True)
+
+        preloaded_list_earth.append(mollviewMapReconstr)
+
 
 def plotAlmMap (i):
-
     LMAX = int(np.exp(i/28.*5.3))
 
-    mollviewMapReconstr = np.load("data/mollviewEarth_recon_max%d.dat"%LMAX,allow_pickle=True)
+    mollviewMapReconstr = preloaded_list_earth[i-1]
     #plotEarth(mollviewMapReconstr)
     #plotPowerspectrum(cl,LMAX)
     plotEarth_Powerspectrum(mollviewMapReconstr,cl,LMAX)
@@ -214,8 +220,8 @@ pbar.update(1)
 
 
 #number of $\ell$-modes that are to be plotted in the triangular plot
-#CAUTION: plot below has been formatted according to maxL = 8; 
-#         maxL can be changed but then plot has to be reformatted 
+#CAUTION: plot below has been formatted according to maxL = 8;
+#         maxL can be changed but then plot has to be reformatted
 maxL = 8
 
 
@@ -227,7 +233,7 @@ shapeSummedMaps = np.append(maxL,np.shape(mapCur))
 summedMaps = np.zeros(shapeSummedMaps)
 
 #formatting paramters that need to be adjusted if maxL is changed
-leftMarginEll       = -700 
+leftMarginEll       = -700
 leftMarginM         = 150
 topMargMfirst       = -140
 topMargMfollow      = 160
@@ -250,7 +256,7 @@ def triangularPlot ():
                     axes[i, j].text(leftMarginM,topMargMfollow,"$m=%d$"%j,size=18)
                 axes[i, j].axis('off')
             else:
-                mapCur = np.loadtxt("data/triangular%d%d.dat"%(i,j)) 
+                mapCur = np.loadtxt("data/triangular%d%d.dat"%(i,j))
                 if(i==0):
                     axes[i, j].text(leftMarginM,topMargMfirst,"$m=%d$"%j,size=18)
                 axes[i, j].imshow(mapCur,cmap="gist_earth")
@@ -260,13 +266,13 @@ def triangularPlot ():
                 mapArray[i,j] = mapCur
         if(i not in [1,2,4]):
             pbarTriang.update(1)
-        
+
     axes[0, maxL].text(leftMarginM,topMargMfirst,r"$\Sigma_{\ell\leq\ell_{cur},m(\ell)}$",size=18)
     for curL in range(maxL):
         curSummedMap = np.zeros_like(mapArray[0,0])
         for j in range(curL+1):
             curSummedMap += np.sum(mapArray[j],axis=0)
-        
+
         axes[curL, maxL].imshow(curSummedMap,cmap='gist_earth')
         axes[curL, maxL].axis('off')
 
@@ -406,25 +412,25 @@ def superpose_spherical_harmonics(l0, l1m0, l1m1, l2m0, l2m1, l2m2, l3m0,l3m1,l3
     map00 = np.loadtxt("data/triangular00.dat")
     superposition = np.zeros_like(map00)
     if l0:
-        superposition += map00
+        superposition += 1.5*map00
     if l1m0:
-        superposition += np.loadtxt("data/triangular10.dat") 
+        superposition += 1.5*np.loadtxt("data/triangular10.dat")
     if l1m1:
-        superposition += np.loadtxt("data/triangular11.dat")
+        superposition += 1.5*np.loadtxt("data/triangular11.dat")
     if l2m0:
-        superposition += np.loadtxt("data/triangular20.dat")
+        superposition += 1.5*np.loadtxt("data/triangular20.dat")
     if l2m1:
-        superposition += np.loadtxt("data/triangular21.dat")
+        superposition += 1.5*np.loadtxt("data/triangular21.dat")
     if l2m2:
-        superposition += np.loadtxt("data/triangular22.dat")
+        superposition += 1.5*np.loadtxt("data/triangular22.dat")
     if l3m0:
-        superposition += np.loadtxt("data/triangular30.dat")
+        superposition += 1.5*np.loadtxt("data/triangular30.dat")
     if l3m1:
-        superposition += np.loadtxt("data/triangular31.dat")
+        superposition += 1.5*np.loadtxt("data/triangular31.dat")
     if l3m2:
-        superposition += np.loadtxt("data/triangular32.dat")
+        superposition += 1.5*np.loadtxt("data/triangular32.dat")
     if l3m3:
-        superposition += np.loadtxt("data/triangular33.dat")
+        superposition += 1.5*np.loadtxt("data/triangular33.dat")
     plotEarth(superposition)
 
 
@@ -444,35 +450,35 @@ def plotCMB(cmbMap):
 
     #plotting map
     im = ax.imshow(cmbMap,vmin=-0.0005, vmax=0.0005)
-    
+
     #setting position and size of colorbar (xPos,yPos,width,height)
-    cax = fig.add_axes([0.32, 0.25, 0.4, 0.018]) 
+    cax = fig.add_axes([0.32, 0.25, 0.4, 0.018])
     cbar = fig.colorbar(im,orientation="horizontal",cax=cax)
-    cbar.set_label('Temperaturschwankung $\Delta T$', size=24, labelpad=20)
+    cbar.set_label('Temperaturschwankung $\Delta T [\text{K}]$', size=24, labelpad=20)
 
     ax.axis('off')
 
 
-    
+
 def plotCMB3d(cmbMap,cmb3d):
-        
+
     fig = plt.figure(figsize=(18,16))
     gs = fig.add_gridspec(1, 2,width_ratios=[1,4])
 
     ax1 = fig.add_subplot(gs[0,0])
-    ax1.imshow(cmb3d) 
+    ax1.imshow(cmb3d)
     ax1.axis('off')
 
     #plotting map
     ax2 = fig.add_subplot(gs[0,1])
     im = ax2.imshow(cmbMap,vmin=-0.0005, vmax=0.0005)
-       
+
     #setting position and size of colorbar (xPos,yPos,width,height)
-    cax = fig.add_axes([0.41, 0.3, 0.4, 0.018]) 
+    cax = fig.add_axes([0.41, 0.3, 0.4, 0.018])
     ticks = np.arange(-0.0004,0.00041,0.0002)
     cbar = fig.colorbar(im,orientation="horizontal",cax=cax,ticks=ticks)
     cax.tick_params(labelsize=20)
-    cbar.set_label('Temperaturschwankung $\Delta T$', size=24, labelpad=20)
+    cbar.set_label('Temperaturschwankung $\Delta T [\text{K}]$', size=24, labelpad=20)
 
     ax2.axis('off')
 
@@ -484,24 +490,24 @@ clCMB = np.loadtxt("data/clCMB.dat")
 ellCMB = np.arange(len(clCMB))[2:]
 
 
-def plotPowerspectrumCMB(clCur,LMAX,isCMB=False):    
+def plotPowerspectrumCMB(clCur,LMAX,isCMB=False):
     fig = plt.figure(figsize=(12,10))
     ax = fig.add_subplot(111)
 
-    #split the ell and cl arrays it two halfes 
-    # s.t. they can be plotted in two different plot styles 
+    #split the ell and cl arrays it two halfes
+    # s.t. they can be plotted in two different plot styles
     ellLeq = ellCMB[ellCMB<=LMAX]
     clDimlessLeq = ellLeq* (ellLeq + 1) * clCur[ellCMB<=LMAX]
     ellGeq = ellCMB[ellCMB>=LMAX]
     clDimlessGeq = ellGeq * (ellGeq + 1) * clCur[ellCMB>=LMAX]
-    
+
     #plot the C_l power spectrum
     ax.plot(ellLeq, clDimlessLeq, c="#222222", lw=1.5)
     ax.plot(ellGeq, clDimlessGeq, c="#666666", lw=1.5)
-    
+
     # add the vertical line and shaded region
     ax.axvline(x=LMAX,lw=2)
-    xLim = np.array(ax.get_xlim())    
+    xLim = np.array(ax.get_xlim())
     xLim[1] = 210
     if (isCMB):
         xLim[1] = 1024
@@ -530,28 +536,28 @@ def fmtCMB(x, pos):
     return r'${}\!\times 10^{{{}}}$'.format(a, b)
 
 
-def plotCMB_Powerspectrum(cmbMap,clCur,LMAX):    
-    #split the ell and cl arrays it two halfes 
-    # s.t. they can be plotted in two different plot styles 
+def plotCMB_Powerspectrum(cmbMap,clCur,LMAX):
+    #split the ell and cl arrays it two halfes
+    # s.t. they can be plotted in two different plot styles
     ellLeq = ellCMB[ellCMB<=LMAX]
     clDimlessLeq = ellLeq* (ellLeq + 1) * clCur[ellCMB<=LMAX]
     ellGeq = ellCMB[ellCMB>=LMAX]
     clDimlessGeq = ellGeq * (ellGeq + 1) * clCur[ellCMB>=LMAX]
 
-    
+
     heights = [1.5, 3, 1.5]
     fig = plt.figure(figsize=(18,10))
     gs1 = fig.add_gridspec(nrows=3, ncols=1, left=0.05, right=0.25,wspace=0.05, height_ratios=heights)
     ax1 = fig.add_subplot(gs1[1,0])
- 
-    
+
+
     #plot the C_l power spectrum
     ax1.plot(ellLeq, clDimlessLeq, c="#222222", lw=1.5)
     ax1.plot(ellGeq, clDimlessGeq, c="#666666", lw=1.5)
-    
+
     # add the vertical line and shaded region
     ax1.axvline(x=LMAX,lw=2)
-    xLim = np.array(ax1.get_xlim())    
+    xLim = np.array(ax1.get_xlim())
     xLim[1] = 1024
 
     ax1.axvspan(LMAX, xLim[1], facecolor='0.2', alpha=0.2)
@@ -561,36 +567,45 @@ def plotCMB_Powerspectrum(cmbMap,clCur,LMAX):
     ax1.set_xlabel(r"$\ell$", size=24)
     ax1.set_ylabel(r"$\ell(\ell+1)C_{\ell}$", size=24)
     ax1.yaxis.set_major_formatter(fmtCMB)
-    
-    
+
+
     plt.title("Leistungsspektrum $C_{\ell}$",fontsize=24)
 
     plt.xticks(fontsize=16)
     plt.yticks(fontsize=16)
     ax1.grid(True)
-    
+
     gs2 = fig.add_gridspec(nrows=1, ncols=1, left=0.30, right=0.98,wspace=0.05)
     ax2 = fig.add_subplot(gs2[0,0])
 
     #plotting map
     im = ax2.imshow(cmbMap,vmin=-0.0005, vmax=0.0005)
-    
+
     #setting position and size of colorbar (xPos,yPos,width,height)
-    cax = fig.add_axes([0.434, 0.16, 0.4, 0.018]) 
+    cax = fig.add_axes([0.434, 0.16, 0.4, 0.018])
     ticks = np.arange(-0.0004,0.00041,0.0002)
     cbar = fig.colorbar(im,orientation="horizontal",cax=cax,ticks=ticks)
     cax.tick_params(labelsize=20)
-    cbar.set_label('Temperaturschwankung $\Delta T$', size=24, labelpad=20)
+    cbar.set_label('Temperaturschwankung $\Delta T [\text{K}]$', size=24, labelpad=20)
     ax2.text(700,10,'$\ell = %d$'%LMAX, size=24)
     ax2.axis('off')
 
 
 
+preloaded_list_cmb = []
+
+for i in range(1, 31):
+    LMAX_cmb = int(np.exp(i/30.*6.9))
+
+    mollviewMapReconstr = np.loadtxt("data/mollviewCMB_recon_max%d.dat"%LMAX_cmb)
+
+    preloaded_list_cmb.append(mollviewMapReconstr)
+
 def plotAlmMapCMB (i):
 
     LMAX = int(np.exp(i/30.*6.9))
 
-    mollviewMapReconstr = np.loadtxt("data/mollviewCMB_recon_max%d.dat"%LMAX)
+    mollviewMapReconstr = preloaded_list_cmb[i-1]
     #plotCMB(mollviewMapReconstr)
     #plotPowerspectrumCMB(clCMB[2:],LMAX,True)
     plotCMB_Powerspectrum(mollviewMapReconstr,clCMB[2:],LMAX)
@@ -634,10 +649,10 @@ def signal(x, a = 0.0, b = 0.0, c=0.0):
 def reconstruct_signal(A = 0.1, B = 0.0, C = 0.0):
     plt.figure(figsize=(13,9))
     x_space = np.linspace(0, 5, 1000)
-    plt.plot(x_space, signal(x_space, a = A), 'b--', linewidth = 0.8, label="Signal 1")
-    plt.plot(x_space, signal(x_space, b = B), 'b-.', linewidth = 0.8, label="Signal 2")
-    plt.plot(x_space, signal(x_space, c = C), 'b:', linewidth = 0.8, label="Signal 3")
-    plt.plot(x_space, signal(x_space, A, B, C), 'k-', linewidth = 1.5, label="Überlagerung 1+2+3")
+    plt.plot(x_space, signal(x_space, a = A), 'b--', linewidth = 0.8, label="A\,$\sin(\pi\cdot x)$")
+    plt.plot(x_space, signal(x_space, b = B), 'b-.', linewidth = 0.8, label="$B\,\sin(2\pi\cdot x)$")
+    plt.plot(x_space, signal(x_space, c = C), 'b:', linewidth = 0.8, label="$C\,\sin(3\pi\cdot x)$")
+    plt.plot(x_space, signal(x_space, A, B, C), 'k-', linewidth = 1.5, label="$f(x)$")
     plt.plot(x_space, signal(x_space, a0, b0, c0), 'r-', linewidth = 2, label="Gemessenes Signal")
     plt.xlabel("$x$")
     plt.ylabel("$f(x)$")
@@ -646,13 +661,13 @@ def reconstruct_signal(A = 0.1, B = 0.0, C = 0.0):
     plt.text(0.8,2.3,"$f(x) = %1.1f \;\sin(\pi \cdot x) + %1.1f \;\sin(2\pi \cdot x)+ %1.1f \;\sin(3\pi \cdot x)$"%(A,B,C), fontsize=18)
     plt.legend(bbox_to_anchor=(1.02, 0.45, 0.5, 0.5))
     plt.show()
-    
+
 pbar.update(1)
 
 
 
 
-########################## PART 4 #############################################################
+########################## PART 5 #############################################################
 
 #Set up a new set of parameters for CAMB
 pars = camb.CAMBparams()
@@ -676,12 +691,12 @@ def createCldata (oBar):
     results = camb.get_results(pars)
     powers =results.get_cmb_power_spectra(pars, CMB_unit='muK')
     totCL_new=powers['total']
-    ls = np.arange(totCL.shape[0])    
+    ls = np.arange(totCL_new.shape[0])
 
     np.savetxt("data/Cl_%.3f.dat"%oBar,(ls,totCL_new[:,0]))
 
 def plotCMBps (oBarPercent):
-      
+
     oBar = oBarPercent/100.
     ls,clCur = np.loadtxt("data/Cl_%.3f.dat"%oBar)
 
@@ -702,9 +717,9 @@ def plotCMBps (oBarPercent):
 
     barPercent = oBar*100
     ax.text(1700,13000*amplitude,"Atome: %.1f%%"%(barPercent/(barPercent/100.+0.1198)),size=28,color='#2966a3')
-    ax.text(1700,11800*amplitude,"Atome: ? ",size=28,color='k')
+    ax.text(1700,11800*amplitude,"Atome: ? ",size=28,color='r')
 
-    
+
 def checkData(minObar,maxObar,stepSize):
     oBar = minObar
     while oBar < maxObar:
@@ -716,10 +731,10 @@ def checkData(minObar,maxObar,stepSize):
 pbar.update(1)
 
 
-# set parameters fro slider widget        
+# set parameters fro slider widget
 minObar = 0.002
 maxObar = 0.2
-stepSize = 0.01
+stepSize = 0.001
 
 #check if all necessary files are available
 #create them if necessary
